91f4c74101a764922aee0e4943696b8378e12bee
Anirban Sikdar <anirban9293@gmail.com>
2025-02-22 23:30:02 +0530

    File structure changes

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,5 +1,7 @@
+"use server";
+
 import Home from "../components/home";
-import { auth0 } from "../../lib/auth0";
+import { auth0 } from "@/lib/auth0";
 import Link from "next/link";
 
 export default async function Page() {
@@ -13,7 +15,7 @@ export default async function Page() {
   }
   return (
     <div className="flex flex-col items-center justify-center w-dvw h-dvh text-2xl">
-      <a className="text-2xl">Hello</a><br/>
+      <a className="text-2xl">Hello - {session.user.name}</a><br/>
       <Link href="/auth/logout" className="h-fit w-fit py-2 px-7 dark:text-black bg-white rounded-full">Logout</Link>
     </div>
   )
